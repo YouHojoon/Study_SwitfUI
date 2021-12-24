bd29ed3b315e1a707bd3e62911ae179246a557df
YouHojoon <dbghwns66@daum.net>
2021-12-24 12:52:23 +0900

    Initial Commit

---
+++ StudySwiftUI/StudySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudySwiftUIApp.swift
+//  StudySwiftUI
+//
+//  Created by 유호준 on 2021/12/24.
+//
+
+import SwiftUI
+
+@main
+struct StudySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
